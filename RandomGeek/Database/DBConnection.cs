c6fbcabfd8908773aa0c1e6f3a94e6324021a1f0
QuickLast <quicklast.com@gmail.com>
2023-11-21 19:17:24 +0300

    Added the structure

---
+++ RandomGeek/Database/DBConnection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomGeek.Database
+{
+    internal class DBConnection
+    {
+    }
+}
