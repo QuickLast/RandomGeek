1ce046e993d1aee465aadde8508d1d56f1d751ed
Сафиуллин Камиль <texnikum.17@gmail.com>
2023-11-21 21:30:19 +0300

    Сверстали страницу авторизации

--- RandomGeek/Pages/AuthorizationPage.xaml.cs
+++ RandomGeek/Pages/AuthorizationPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using RandomGeek.Pages;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,17 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace RandomGeek
+namespace RandomGeek.Pages
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для AuthorizationPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AuthorizationPage : Page
     {
-        public MainWindow()
+        public AuthorizationPage()
         {
             InitializeComponent();
-            navFr.NavigationService.Navigate(new AuthorizationPage());
         }
     }
 }
