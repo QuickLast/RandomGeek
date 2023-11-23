105ca1bb5e30d70610953a40c405cb24f4e31824
№2 Бородатый джо <zagir-eva@mail.ru>
2023-11-23 21:51:14 +0300

    Начали делать страницу добавления кино

--- RandomGeek/Pages/AdminPage.xaml.cs
+++ RandomGeek/Pages/AdminPage.xaml.cs
@@ -23,12 +23,12 @@ namespace RandomGeek.Pages
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void AddMovieBtn_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new AddMoviePage());
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void AddGameBtn_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new AddGamePage());
         }
