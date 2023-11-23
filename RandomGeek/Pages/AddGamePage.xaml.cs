0ec5acbe3495999587a39c0b46f0eeb59d9103c4
№2 Бородатый джо <zagir-eva@mail.ru>
2023-11-23 21:19:49 +0300

    Связали страницу для админа со страницей добавления нового кино.

--- RandomGeek/Pages/AddGamePage.xaml.cs
+++ RandomGeek/Pages/AddGamePage.xaml.cs
@@ -15,17 +15,14 @@ using System.Windows.Shapes;
 
 namespace RandomGeek.Pages
 {
-  
-    public partial class AdminPage : Page
+    /// <summary>
+    /// Логика взаимодействия для AddGamePage.xaml
+    /// </summary>
+    public partial class AddGamePage : Page
     {
-        public AdminPage()
+        public AddGamePage()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            NavigationService.Navigate(new AddMoviePage());
         }
     }
 }
