e3aac7f18b9faa0e47bff7c2228e14a591b34f1c
Сафиуллин Камиль <texnikum.17@gmail.com>
2023-11-24 19:51:26 +0300

    Сделали регистрацию, изменили и переподключили базу данных

--- RandomGeek/Database/User.cs
+++ RandomGeek/Database/User.cs
@@ -17,5 +17,6 @@ namespace RandomGeek.Database
         public int IDUser { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public string Name { get; set; }
     }
 }
--- RandomGeek/Pages/RegistrationPage.xaml.cs
+++ RandomGeek/Pages/RegistrationPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using RandomGeek.Database;
 
 namespace RandomGeek.Pages
 {
@@ -27,7 +28,29 @@ namespace RandomGeek.Pages
 
         private void RegBtn_Click(object sender, RoutedEventArgs e)
         {
-            NavigationService.Navigate(new MainPage());
+            if (EmailTb.Text == string.Empty || NameTb.Text == string.Empty || PasswordPb.Password == string.Empty)
+            {
+                MessageBox.Show("Заполните все поля!");
+            }
+            else
+            {
+                User user = new User()
+                {
+                    Email = EmailTb.Text,
+                    Name = NameTb.Text,
+                    Password = PasswordPb.Password
+                };
+
+                DbConnection.RandomGeek_KamilEntities.User.Add(user);
+                DbConnection.RandomGeek_KamilEntities.SaveChanges();
+
+                MessageBox.Show("Регистрация прошла успешно!");
+
+                NavigationService.Navigate(new MainPage());
+            }
+            
+            
+            
         }
     }
 }
